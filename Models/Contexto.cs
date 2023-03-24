15d2afb733e83606e3813b7e62ffe9b344e39e45
Lucas Alves <luc.bado@gmail.com>
2023-03-24 19:47:17 -0300

    Criação do app

---
+++ Models/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BadBitch.Models
+{
+    public class Contexto : DbContext
+    {
+
+ public Contexto( DbContextOptions<Contexto> options):base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+
+    }
+}
